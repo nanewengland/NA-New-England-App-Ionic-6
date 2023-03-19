826bc6874971c97f620c5c255c13f0e01408bf4d
Patrick Joyce <pjaudiomv@gmail.com>
2023-03-19 10:56:45 -0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  nernaBMLT: 'https://nerna.org/main_server/client_interface/json/',
+  tomatoBMLT: 'https://tomato.bmltenabled.org/main_server/client_interface/json/',
+  getApiUrlServiceGroups: 'https://tomato.bmltenabled.org/main_server/client_interface/json/?switcher=GetServiceBodies&services=666&recursive=1',
+  wordpressApiUrl: 'https://nerna.org/nerna_scripts/nernacal/nerna-events-app.php',
+  tomatoNernaBMLT: 'https://tomato.bmltenabled.org/main_server/client_interface/json/?switcher=GetSearchResults&services=666&recursive=1'
+};
